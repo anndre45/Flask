232fb666bf8b5e2894471d14a2936c6e9ff0694f
André Almeida <ajalmeida3@gmail.com>
2025-04-29 22:20:40 -0300

    basic auth

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
 from flask import Flask, jsonify, request
+from flask_httpauth import HTTPBasicAuth
 
 app = Flask(__name__)
 
+auth = HTTPBasicAuth()
+
+users = {
+    "user1": "password1",
+    "user2": "password2"
+}
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and users[username] == password:
+        return username
+
 @app.route('/')
+@auth.login_required
 def home():
     return "Hello, Flask!"
 
